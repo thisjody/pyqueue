7ac8521662804cddb58403a626995ab0d5973ae1
jody <thisjody@gmail.com>
2013-07-17 13:08:57 -0700

    add __cmp__

--- pyqueue.py
+++ pyqueue.py
@@ -59,4 +59,13 @@ class Node:
     def __str__(self):
         return str(self.cargo)
 
-
+class Golfer:
+    def __init__(self, name, score):
+        self.name = name
+        self.score= score
+    def __str__(self):
+        return "%-16s: %d" % (self.name, self.score)
+    def __cmp__(self, other):
+        if self.score < other.score: return 1 # less is more
+        if self.score > other.score: return -1
+        return 0
