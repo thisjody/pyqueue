42306fa4a36dd233ac64a690c3edf77f7a12e351
jody <thisjody@gmail.com>
2013-07-17 12:36:40 -0700

    adding priorty q

---
+++ pyqueue.py
@@ -0,0 +1,62 @@
+class ImprovedQueue:
+    def __init__(self):
+        self.length = 0
+        self.head = None
+        self.last = None
+
+    def isEmpty(self):
+        return (self.length == 0)
+
+    def insert(self, cargo):
+        node = Node(cargo)
+        node.next = None
+        if self.length == 0:
+            # if list is empty, the new node is head and last
+            self.head = self.last = node
+        else:
+            # find the last node
+            last = self.last
+            # append the new node
+            last.next = node
+            self.last = node
+        self.length = self.length + 1
+        print self.length
+
+    def remove(self):
+        cargo = self.head.cargo
+        self.head = self.head.next
+        self.length = self.length - 1
+        if self.length == 0:
+            self.last = None
+        return cargo
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def insert(self, item):
+        self.items.append(item)
+
+    def remove(self):
+        maxi = 0
+        for i in range(1,len(self.items)):
+            if self.items[i] > self.items[maxi]:
+                maxi = i
+        item = self.items[maxi]
+        self.items[maxi:maxi+1] = []
+        return item
+
+        
+class Node:
+    def __init__(self, cargo=None, next=None):
+        self.cargo = cargo
+        self.next = next
+
+    def __str__(self):
+        return str(self.cargo)
+
+
